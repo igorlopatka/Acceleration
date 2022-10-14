721ff61e2ca40bd186f712574923b19fa50a629b
Igor Łopatka <igor.lopatka11@gmail.com>
2022-10-14 21:58:41 +0200

    Add SoundManager for playing sound on run finish

--- Acceleration/Model/SoundManager.swift
+++ Acceleration/Model/SoundManager.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import AVKit
+
+class SoundManager: ObservableObject {
+    
+    @Published var player: AVAudioPlayer!
+    
+    let finishSound = Bundle.main.path(forResource: "finish", ofType: "mp3")
+    
+    func playFinishSound() {
+        self.player = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: finishSound!))
+    }
+}
