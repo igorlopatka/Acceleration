ae0251c4ced6a78e2de005a1e054faa0ff918a76
Igor Łopatka <igor.lopatka11@gmail.com>
2022-12-16 21:53:34 +0100

    Create viewmodel for app

---
+++ Acceleration/ViewModel/AccelerationViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  AccelerationViewModel.swift
+//  Acceleration
+//
+//  Created by Igor Łopatka on 16/12/2022.
+//
+
+import Foundation
+
+class AccelerationViewModel: ObservableObject {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
