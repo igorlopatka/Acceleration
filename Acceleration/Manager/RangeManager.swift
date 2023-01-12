f8dde3e71c22603fe781df1ac9c2f367dd8c46ab
Igor Łopatka <igor.lopatka11@gmail.com>
2023-01-12 20:25:55 +0100

    add UserDefaults for range settings

--- Acceleration/Manager/RangeManager.swift
+++ Acceleration/Manager/RangeManager.swift
@@ -5,15 +5,15 @@
 //  Created by Igor Łopatka on 12/04/2022.
 //
 
-import Foundation
+import SwiftUI
 
 class RangeManager: ObservableObject {
     
-    @Published var start = 0
-    @Published var finish = 100
-    @Published var optRunActive = false
-    @Published var optStart = 100
-    @Published var optFinish = 200
+    @AppStorage("start") var start = 0
+    @AppStorage("finish") var finish = 100
+    @AppStorage("optRunActive") var optRunActive = false
+    @AppStorage("optStart") var optStart = 100
+    @AppStorage("optFinish") var optFinish = 200
     
     let values = [0,20,40,60,80,100,120,140,160,180,200,220,240,260,280,300]
 }
