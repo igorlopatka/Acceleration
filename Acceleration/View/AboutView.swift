1ad9a3b442147d7c47df4cf1a5518e86c28170f4
Igor Łopatka <igor.lopatka11@gmail.com>
2023-02-16 18:50:38 +0100

    Create about view

---
+++ Acceleration/View/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  Acceleration
+//
+//  Created by Igor Łopatka on 16/02/2023.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
