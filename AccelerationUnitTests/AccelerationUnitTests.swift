f8a51d79c4736744639ba896fd0cdac35c57d617
Igor Łopatka <igor.lopatka11@gmail.com>
2023-02-17 21:15:02 +0100

    add unit tests target

--- AccelerationUnitTests/AccelerationUnitTests.swift
+++ AccelerationUnitTests/AccelerationUnitTests.swift
@@ -6,9 +6,17 @@
 //
 
 import XCTest
+@testable import Acceleration
 
 final class AccelerationUnitTests: XCTestCase {
-
+    
+    /*
+    XCTAssertNotNil asserts a variable is not nil.
+    XCTAssertTrue asserts a condition is true.
+    XCTAssertFalse asserts a condition is false.
+    XCTAssertEqual asserts two values are equal.
+    */
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,19 +25,6 @@ final class AccelerationUnitTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
