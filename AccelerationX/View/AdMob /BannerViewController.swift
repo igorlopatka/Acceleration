f1f9cfad03479627ceaec2e994fa734c879763d0
Igor Łopatka <igor.lopatka11@gmail.com>
2023-06-11 20:19:32 +0200

    Fix to AdMob, add test targets

--- AccelerationX/View/AdMob /BannerViewController.swift
+++ AccelerationX/View/AdMob /BannerViewController.swift
@@ -9,23 +9,29 @@ import Foundation
 import UIKit
 
 class BannerViewController: UIViewController {
-    
-    weak var delegate: BannerViewControllerWidthDelegate?
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        delegate?.bannerViewController(self, didUpdate: view.frame.inset(by: view.safeAreaInsets).size.width)
+  weak var delegate: BannerViewControllerWidthDelegate?
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    // Tell the delegate the initial ad width.
+    delegate?.bannerViewController(
+      self, didUpdate: view.frame.inset(by: view.safeAreaInsets).size.width)
+  }
+
+  override func viewWillTransition(
+    to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator
+  ) {
+    coordinator.animate { _ in
+      // do nothing
+    } completion: { _ in
+      // Notify the delegate of ad width changes.
+      self.delegate?.bannerViewController(
+        self, didUpdate: self.view.frame.inset(by: self.view.safeAreaInsets).size.width)
     }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        coordinator.animate { _ in
-            
-        } completion: { _ in
-            self.delegate?.bannerViewController(self, didUpdate: self.view.frame.inset(by: self.view.safeAreaInsets).size.width)
-        }
-    }
+  }
 }
 
 protocol BannerViewControllerWidthDelegate: AnyObject {
-    func bannerViewController(_ bannerViewController: BannerViewController, didUpdate width: CGFloat)
+  func bannerViewController(_ bannerViewController: BannerViewController, didUpdate width: CGFloat)
 }
